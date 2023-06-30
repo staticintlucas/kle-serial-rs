7f61b460a1b43a72e503b5af35f908eb2a50a5de
Lucas Jansen <7199136+staticintlucas@users.noreply.github.com>
2023-06-30 16:48:31 +0100

    Add support for key rotation with r, rx, and ry properties

--- src/de/mod.rs
+++ src/de/mod.rs
@@ -12,6 +12,9 @@ pub(crate) use json::{KleKeyboard, KleLegendsOrProps, KlePropsObject};
 #[derive(Debug, Default)]
 #[allow(clippy::struct_excessive_bools)]
 pub(crate) struct KleProps {
+    // Internal fields
+    is_first: bool, // r, rx, & ry can only be set in the first properties object of a line
+
     // Per-key properties
     x: f64,
     y: f64,
@@ -30,6 +33,9 @@ pub(crate) struct KleProps {
     d: bool, // decal
 
     // Persistent properties
+    r: f64,
+    rx: f64,
+    ry: f64,
     g: bool,    // ghosted
     sm: String, // switch mount
     sb: String, // switch brand
@@ -72,9 +78,22 @@ impl KleProps {
             std::array::from_fn(|i| ta.get(i).copied().flatten().unwrap_or(t))
         });
 
+        // KLE has some weird rotation behaviour, with rx and ry (if present) resetting x and y
+        // Also note: KLE only allows r, rx, ry at the start of the line. We don't enforce this,
+        // but expect some weird behaviour if you try to use it
+        let (x, y, rx, ry) = if props.rx.is_some() || props.ry.is_some() {
+            let rx = props.rx.unwrap_or(self.rx);
+            let ry = props.ry.unwrap_or(self.ry);
+            (rx, ry, rx, ry)
+        } else {
+            (self.x, self.y, self.rx, self.ry)
+        };
+
+        // Internal fields
+        self.is_first = false;
         // Per-key properties
-        self.x += props.x.unwrap_or(0.0);
-        self.y += props.y.unwrap_or(0.0);
+        self.x = x + props.x.unwrap_or(0.0);
+        self.y = y + props.y.unwrap_or(0.0);
         self.w = props.w.unwrap_or(1.);
         self.h = props.h.unwrap_or(1.);
         self.x2 = props.x2.unwrap_or(0.);
@@ -85,6 +104,9 @@ impl KleProps {
         self.n = props.n.unwrap_or(false);
         self.d = props.d.unwrap_or(false);
         // Persistent properties
+        self.r = props.r.unwrap_or(self.r);
+        self.rx = rx;
+        self.ry = ry;
         self.g = props.g.unwrap_or(self.g);
         self.sm = props.sm.unwrap_or(self.sm.clone());
         self.sb = props.sb.unwrap_or(self.sb.clone());
@@ -117,7 +139,8 @@ impl KleProps {
     #[inline]
     pub(crate) fn next_line(&mut self) {
         self.next_key();
-        self.x = 0.;
+        self.is_first = true;
+        self.x = self.rx; // x resets to rx
         self.y += 1.;
     }
 
@@ -142,9 +165,9 @@ impl KleProps {
             y2: self.y2,
             w2: self.w2,
             h2: self.h2,
-            rotation: 0., // TODO: self.r
-            rx: 0.,       // TODO: self.rx
-            ry: 0.,       // TODO: self.ry
+            rotation: self.r,
+            rx: self.rx,
+            ry: self.ry,
             profile: self.p.clone(),
             ghosted: self.g,
             switch: Switch {
